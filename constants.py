ad09afc8c40067864397c8333c5ed0b86aa3f733
Tamara Klochkova <Tamara.Klochkova@data3.ru>
2021-10-31 11:48:21 +0300

    Третье домашнее задание

--- constants.py
+++ constants.py
@@ -1,23 +1,22 @@
 NEGATIVE_LOGIN_CREDENTIALS = [
-   ("", "!QAZ2wsx"),
-   ("qa_test@test.ru", ""),
-   ("qa_test", "!QAZ2wsx"),
-   ("test@test.ru", "1QAZ2wsx")
+    ("", "!QAZ2wsx"),
+    ("qa_test@test.ru", ""),
+    ("qa_test", "!QAZ2wsx"),
+    ("test@test.ru", "1QAZ2wsx")
 ]
 
-
-POSITIVE_LOGIN_CREDENTIALS = {"email": "qa_test@test.ru",
-                             "password": "!QAZ2wsx"}
+POSITIVE_LOGIN_CREDENTIALS = {"email": "api_user_7@test.ru",
+                              "password": "q7w7e7"}
 
 
 class Links:
-   base_url = "https://qastand.valhalla.pw/"
-   login = base_url + "login"
-   profile = base_url + "profile"
-   blog = base_url + "blog"
+    base_url = {"prod": "https://qastand.valhalla.pw/",
+                "stage": "https://qastand-dev.valhalla.pw/"}
+    login = "login"
+    profile = "profile"
+    blog = "blog"
 
 
 SESSION_COOKIE = {'name': 'session',
-                 'value': '.eJwlzjsOwjAMANC7ZGawHX-SXqaKY0ewtnRC3J1KTG99n7KvI89n2d7HlY-yv6JsBaMOY1RaQNzqSp05QsSpazaXI'                  'FATqiOH1qnCbI3SQRHSAmG26k1o9NWhMknrWN37jU0IU45wme5GiyWBHLkj8mzc09y03JHrzOO_ofL9AVk6Le8.YVn0'
-'oA.WT_USo8F4bTkxwenEGx0DU0ZzwQ'}
-
+                  'value': '.eJwlzjsOwjAMANC7ZGawHX-SXqaKY0ewtnRC3J1KTG99n7KvI89n2d7HlY-yv6JsBaMOY1RaQNzqSp05QsSpazaXI'                  'FATqiOH1qnCbI3SQRHSAmG26k1o9NWhMknrWN37jU0IU45wme5GiyWBHLkj8mzc09y03JHrzOO_ofL9AVk6Le8.YVn0'
+                           'oA.WT_USo8F4bTkxwenEGx0DU0ZzwQ'}
